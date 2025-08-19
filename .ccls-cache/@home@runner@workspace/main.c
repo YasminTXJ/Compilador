4d489360bd421bc5b3731a9ab27ea102751f152c
YasminTXJ <99887005+YasminTXJ@users.noreply.github.com>
2025-08-19 18:50:36 +0000

    - Codigo apresentado para o Daniel

--- .ccls-cache/@home@runner@workspace/main.c
+++ .ccls-cache/@home@runner@workspace/main.c
@@ -1,3 +1,5 @@
+// Alunos: YASMIN TEIXEIRA DE JESUS E DANIEL ANTÔNIO MIRANDA
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -35,7 +37,7 @@ char *safe_strdup(const char *s);
 size_t total_memory_used = 0;
 size_t max_memory_used = 0;
 size_t memory_limit = DEFAULT_MEMORY_LIMIT;
-
+char escopo_atual[50] = "global";
 // Tokens fixos (constantes literais — não precisam de malloc)
 const char *palavrasReservadas[] = {"principal", "funcao", "retorno", "leia",
                                     "escreva",   "se",     "senao",   "para"};
@@ -43,33 +45,66 @@ const char *tiposDeDados[] = {"inteiro", "texto", "decimal"};
 const char *operadores[] = {"==", "<>", "<=", ">=", "&&", "||", "+",
                             "-",  "*",  "/",  "^",  "<",  ">",  "="};
 const char *marcadores[] = {"(", ")", "{", "}", "[", "]", ";", ",", " "};
+// Estrutura da tabela de símbolos
+typedef struct Simbolo {
+  char tipo[20];
+  char nome[50];
+  char valor[100];
+  char escopo[50];
+  struct Simbolo *prox;
+} Simbolo;
+Simbolo *tabela_simbolos = NULL;
+char tipo_atual[20];
+char nome_atual[50];
+char valor_atual[100];
+
+// Função para buscar símbolo
+Simbolo *buscarSimbolo(const char *nome, const char *escopo) {
+  Simbolo *atual = tabela_simbolos;
+  while (atual != NULL) {
+    if (strcmp(atual->nome, nome) == 0 && strcmp(atual->escopo, escopo) == 0) {
+      return atual;
+    }
+    atual = atual->prox;
+  }
+  return NULL;
+}
+Simbolo *adicionarSimbolo(Simbolo *inicio, const char *tipo, const char *nome,
+                          const char *valor, const char *funcao) {
+  Simbolo *novo = (Simbolo *)malloc(sizeof(Simbolo));
+  if (!novo) {
+    printf("Memória insuficiente!\n");
+    exit(1);
+  }
+  strcpy(novo->tipo, tipo);
+  strcpy(novo->nome, nome);
+  strcpy(novo->valor, valor);
+  strcpy(novo->escopo, funcao);
+  novo->prox = NULL;
+
+  if (!inicio) {
+    return novo;
+  }
+
+  Simbolo *temp = inicio;
+  while (temp->prox)
+    temp = temp->prox;
+  temp->prox = novo;
+  return inicio;
+}
+void imprimirTabela(Simbolo *inicio) {
+  printf("\n%-10s %-15s %-15s %-20s\n", "Tipo", "Nome", "Valor", "Função");
+  printf("-------------------------------------------------------------\n");
+  while (inicio) {
+    printf("%-10s %-15s %-15s %-20s\n", inicio->tipo, inicio->nome,
+           inicio->valor, inicio->escopo);
+    inicio = inicio->prox;
+  }
+}
 
 // Vetor dinâmico de tokens
 char **tokens = NULL;
 int tokenCount = 0;
-
-int main() {
-  int numTokens;
-  char **resultado = tokenizeFile("Erro_exemplo_9.txt", &numTokens);
-
-  if (VerificaSintaxeEhValida(resultado, numTokens)) {
-    printf("-={********************************************************}=-\n");
-    printf("\t\tCodigo Compilado com Sucesso\n");
-    printf("-={********************************************************}=-\n");
-  }
-
-  printf("\n[Informacoes]\nTokens encontrados: %d\n", numTokens);
-  for (int i = 0; i < numTokens; i++) {
-    // printf("Token[%d]: %s\n", i, resultado[i]);
-    LiberaMallocELiberaMemoria(resultado[i], strlen(resultado[i]) + 1);
-  }
-  LiberaMallocELiberaMemoria(resultado, numTokens * sizeof(char *));
-
-  printf("\n[MEMORIA]\n");
-  printf("Maximo de memoria usada: %.2f KB\n", (float)max_memory_used / KB);
-
-  return 0;
-}
 
 //******-------------------------------------------------**********************----------------------------------------------------
 //           Transforma Arquivo TXT em tokens
@@ -336,13 +371,72 @@ int ehMarcador(const char *token) {
   }
   return 0;
 }
+int processarDeclaracao(char **tokens, int i, char *tipo_atual,
+                        char *escopo_atual) {
+
+  // Pega o nome da variável
+  if (tokens[i + 1] != NULL && ehVariavel(tokens[i + 1])) {
+    strcpy(nome_atual, tokens[i + 1]);
+    printf("nome atual = %s\n", nome_atual);
+  }
+
+  // Verifica se é decimal (ex: car[2.5])
+  if (tokens[i + 2] != NULL && ehMarcador(tokens[i + 2]) &&
+      strcmp(tokens[i + 2], "[") == 0) {
+    strcat(nome_atual, tokens[i + 2]); // Adiciona o '['
+    int j = i + 3;
+    while (tokens[j] != NULL && strcmp(tokens[j], "]") != 0) {
+      strcat(nome_atual, tokens[j]); // Adiciona conteúdo entre os colchetes
+      j++;
+    }
+    if (tokens[j] != NULL && strcmp(tokens[j], "]") == 0) {
+      strcat(nome_atual, "]"); // Fecha o colchete
+      i = j - 1;               // Avança o índice para depois do colchete
+    }
+  }
+
+  // Verifica se há um operador "=" após o nome
+  if (tokens[i + 2] != NULL && ehOperador(tokens[i + 2]) &&
+      strcmp(tokens[i + 2], "=") == 0) {
+    printf("eh operador = %s\n", tokens[i + 2]);
+    strcpy(valor_atual, tokens[i + 3]);
+
+    int j = i + 4;
+    while (tokens[j] != NULL && !ehMarcador(tokens[j])) {
+      strcat(valor_atual, tokens[j]); // Concatena valor
+      j++;
+    }
+
+    printf("valoooor atual = %s\n", valor_atual);
+    tabela_simbolos = adicionarSimbolo(tabela_simbolos, tipo_atual, nome_atual,
+                                       valor_atual, escopo_atual);
+    printf("adicionado na tabela de simbolos\n");
+    return j - 1;
+
+  } else if (tokens[i + 2] != NULL && ehMarcador(tokens[i + 2])) {
+    printf("token , = %s\n", tokens[i + 2]);
+    tabela_simbolos = adicionarSimbolo(tabela_simbolos, tipo_atual, nome_atual,
+                                       "", escopo_atual);
+    printf("adicionado na tabela de simbolos sem valor\n");
+
+    // verifica se tem virgula e continua a declaracao de mais variaveis
+    if (tokens[i + 2] != NULL && strcmp(tokens[i + 2], ",") == 0) {
+      printf("tem virgula\n ");
+      int valor = processarDeclaracao(tokens, i + 2, tipo_atual, escopo_atual);
+      return valor;
+    }
+    return i + 2;
+  }
+
+  return i;
+}
 
 int VerificaSintaxeEhValida(char **tokens, int numTokens) {
   int linhaAtual = 1;
 
   for (int i = 0; i < numTokens; i++) {
     const char *token = tokens[i];
-    //printf("Analisando token : %s\n", token);
+    printf("Analisando token : %s\n", token);
     if (strcmp(token, " ") == 0) {
       continue;
     }
@@ -355,21 +449,86 @@ int VerificaSintaxeEhValida(char **tokens, int numTokens) {
       linhaAtual++;
 
     if (ehPalavraReservada(token)) {
-      //printf("eh palavra reservada - token %s\n", token);
+      if (strcmp(token, "principal") == 0) {
+        strcpy(escopo_atual, "principal");
+        printf("ecopo atual = %s\n", escopo_atual);
+      }
+      // printf("eh palavra reservada - token %s\n", token);
     } else if (ehTipoDeDado(token)) {
-      //printf("eh tipo de dado - token %s\n", token);
+      strcpy(tipo_atual, token);
+      printf("tipo atual = %s\n", tipo_atual);
+      processarDeclaracao(tokens, i, tipo_atual, escopo_atual);
+      // variavel declarada
+      /*
+      if (tokens[i + 1] != NULL && ehVariavel(tokens[i + 1])) {
+        strcpy(nome_atual, tokens[i + 1]);
+
+      }
+      //se for decimal car[2.5]
+      if(tokens[i + 2] != NULL && ehMarcador(tokens[i + 2]) &&
+          strcmp(tokens[i + 2], "[") == 0){
+          strcat(nome_atual, tokens[i+2]);
+          while (tokens[i + 3] != NULL && strcmp(tokens[i + 3], "]") == 1){
+            strcat(nome_atual, tokens[i+3]);  // pego o nome com o tamanho
+            i++;
+          }
+      }
+      if (tokens[i + 2] != NULL && ehOperador(tokens[i + 2]) &&
+          strcmp(tokens[i + 2], "=") == 0) {
+        printf("eh operador = %s\n", tokens[i + 2]);
+        strcpy(valor_atual, tokens[i + 3]);
+        while (tokens[i + 4] != NULL && !ehMarcador(tokens[i + 4])){
+          printf("valoooor atual = %s\n", valor_atual);
+           strcat(valor_atual, tokens[i+4]);  // Concatena o token atual
+          i++;
+        }
+        // adiciono a variavel com valor na tabela de simbolos
+        tabela_simbolos = adicionarSimbolo(
+            tabela_simbolos, tipo_atual, nome_atual, valor_atual, escopo_atual);
+        printf("adicionado na tabela de simbolos\n");
+      }else{
+        if (tokens[i + 2] != NULL && ehMarcador(tokens[i + 2]) ) {
+          printf("token , = %s\n", tokens[i + 2]);
+          // adiciono a variavel sem valor na tabela de simbolos
+          tabela_simbolos = adicionarSimbolo(tabela_simbolos, tipo_atual,
+                                             nome_atual, "", escopo_atual);
+          printf("adicionado na tabela de simbolos sem valor\n");
+
+        }
+
+      }*/
+      printf("token + 1  %s\n", tokens[i + 1]);
+      printf("token + 2  %s\n", tokens[i + 2]);
+      printf("token + 3  %s\n", tokens[i + 3]);
+
+      // printf("eh tipo de dado - token %s\n", token);
     } else if (ehFuncao(token)) {
-      //printf("eh funcao - token %s\n", token);
-    } else if (ehVariavel(token)) {
-      //printf("eh variavel - token %s\n", token);
+      strcpy(escopo_atual, token);
+      // printf("eh funcao - token %s\n", token);
+    } else if (ehVariavel(token)) { /*
+       if (tokens[i + 1] != NULL && ehOperador(tokens[i + 1]) &&
+         strcmp(tokens[i + 1], "=") == 0) {
+         strcpy(nome_atual, tokens[i]);
+         printf("nome atual = %s\n", nome_atual);
+         strcpy(valor_atual, tokens[i + 2]);
+         while (tokens[i + 3] != NULL && !ehMarcador(tokens[i + 3])){
+           printf("valoooor atual = %s\n", valor_atual);
+            strcat(valor_atual, tokens[i + 3]);  // Concatena o token atual
+           i++;
+         }
+         tabela_simbolos = adicionarSimbolo(
+             tabela_simbolos, tipo_atual, nome_atual, valor_atual,
+       escopo_atual); printf("adicionado na tabela de simbolos\n");
+       }*/
+      // printf("eh variavel - token %s\n", token);
     } else if (ehNumero(token)) {
-      //printf("eh numero - token %s\n", token);
+      // printf("eh numero - token %s\n", token);
     } else if (ehString(token)) {
-      //printf("eh string - token %s\n", token);
+      // printf("eh string - token %s\n", token);
     } else if (ehOperador(token)) {
-      //printf("eh operador - token %s\n", token);
+      // printf("eh operador - token %s\n", token);
     } else if (ehMarcador(token)) {
-      //printf("eh marcador - token %s\n", token);
+      // printf("eh marcador - token %s\n", token);
     } else {
       printf("------------------------------------------------------\n");
       printf("\tERRO: Sintaxe invalida ('%s')- LINHA %d \n", token, linhaAtual);
@@ -416,4 +575,28 @@ void LiberaMallocELiberaMemoria(void *ptr, size_t size) {
       total_memory_used -= size;
     }
   }
+}
+
+int main() {
+  int numTokens;
+  char **resultado = tokenizeFile("Codigo1.txt", &numTokens);
+
+  if (VerificaSintaxeEhValida(resultado, numTokens)) {
+    printf("-={********************************************************}=-\n");
+    printf("\t\tCodigo Compilado com Sucesso\n");
+    printf("-={********************************************************}=-\n");
+    imprimirTabela(tabela_simbolos);
+  }
+
+  printf("\n[Informacoes]\nTokens encontrados: %d\n", numTokens);
+  for (int i = 0; i < numTokens; i++) {
+    // printf("Token[%d]: %s\n", i, resultado[i]);
+    LiberaMallocELiberaMemoria(resultado[i], strlen(resultado[i]) + 1);
+  }
+  LiberaMallocELiberaMemoria(resultado, numTokens * sizeof(char *));
+
+  printf("\n[MEMORIA]\n");
+  printf("Maximo de memoria usada: %.2f KB\n", (float)max_memory_used / KB);
+
+  return 0;
 }
